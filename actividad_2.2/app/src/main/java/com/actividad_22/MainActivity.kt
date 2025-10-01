72d37cf391329ada13c510140211017bec611a93
hckblack84 <luisparedes.c65@gmail.com>
2025-10-01 20:10:06 -0300

    Actividad 2.2 Terminada

--- actividad_2.2/app/src/main/java/com/actividad_22/MainActivity.kt
+++ actividad_2.2/app/src/main/java/com/actividad_22/MainActivity.kt
@@ -1,4 +1,6 @@
 package com.actividad_22
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,9 +11,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.actividad_22.navigation.NavigationEvent
+import com.actividad_22.navigation.Screen
 import com.actividad_22.ui.theme.Actividad_22Theme
+import com.actividad_22.viewmodels.HomeScreen
+import com.actividad_22.viewmodels.MainViewModel
+import com.actividad_22.viewmodels.ProfileScreen
+import com.actividad_22.viewmodels.SettingsScreen
+import kotlinx.coroutines.flow.collectLatest
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +33,56 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Actividad_22Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+
+
+                val  viewModel: MainViewModel = viewModel()
+                val navController = rememberNavController()
+
+                LaunchedEffect(key1 = Unit) {
+
+                    viewModel.navigationEvents.collectLatest{
+                        event -> when (event){
+                            is NavigationEvent.Navigateto ->{
+                                navController.navigate(event.route.route){
+                                    event.popUpToRoute?.let {
+                                        popUpTo(it.route){
+                                            inclusive=event.inclusive
+                                        }
+                                    }
+                                    launchSingleTop= event.singleTop
+                                    restoreState=true
+                                }
+                            }
+                        is NavigationEvent.popBackStack -> navController.popBackStack()
+                        is NavigationEvent.NavigateUp -> navController.navigateUp()
+                        }
+                    }
+        }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ){ innerPadding ->
+
+                    NavHost(
+                        navController= navController,
+                        startDestination = Screen.Home.route,
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ){
+
+                        composable(route = Screen.Home.route) {
+                            HomeScreen(navController = navController, viewModel = viewModel)
+                        }
+
+                        composable(route = Screen.Profile.route) {
+                            ProfileScreen(navController = navController, viewModel = viewModel)
+                        }
+
+                        composable(route = Screen.Settings.route) {
+                            SettingsScreen(navController = navController, viewModel = viewModel)
+                        }
+                    }
                 }
-            }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Actividad_22Theme {
-        Greeting("Android")
-    }
-}
